323727a91bdc4ad2218dd929da35448c2ead68e3
Ryan Sturgess <ryanleo.nz.rsg@gmail.com>
2025-09-02 11:01:36 +1200

    fix: TimescaleDB setup

--- go.mod
+++ go.mod
@@ -1,8 +1,6 @@
 module pizza-nz/realtime-analytics-dashboard
 
-go 1.23.0
-
-toolchain go1.23.12
+go 1.25.0
 
 require (
 	github.com/klauspost/compress v1.18.0 // indirect
--- sql/001__create_analytic_events.sql
+++ sql/001__create_analytic_events.sql
@@ -1,6 +1,12 @@
 CREATE TABLE IF NOT EXISTS analytics_events (
-    EventType varchar(255),
-    UserID bigint,
-    Created_at timestamptz NOT NULL,
-    EventData jsonb
+    event_type varchar(255),
+    user_id bigint,
+    created_at timestamptz NOT NULL,
+    event_data jsonb,
+    event_id UUID DEFAULT gen_random_UUID()
 );
+
+ALTER TABLE analytics_events 
+ADD PRIMARY KEY (event_id, created_at);
+
+SELECT create_hypertable('analytics_events', 'created_at');
